3074c0ecc6101ead178b167902074a17f5a5411d
XD_CZ <mandlemankiller@email.cz>
2022-12-20 19:56:27 +0100

    Implement cooldowns in fire charge interact

--- src/cz/jeme/programu/mobi/morphs/Ghast.java
+++ src/cz/jeme/programu/mobi/morphs/Ghast.java
@@ -6,14 +6,18 @@ import org.bukkit.entity.Fireball;
 import org.bukkit.entity.Player;
 import org.bukkit.event.player.PlayerInteractEvent;
 
+import cz.jeme.programu.mobi.CooldownManager;
 import cz.jeme.programu.mobi.interfaces.Flyable;
 import cz.jeme.programu.mobi.interfaces.Interactable;
 import net.md_5.bungee.api.ChatColor;
 
 public class Ghast extends Morph implements Interactable, Flyable {
 
+	private CooldownManager cooldownManager;
+
 	public Ghast() {
 		setMaxHealth(10);
+		this.cooldownManager = new CooldownManager();
 	}
 
 	@Override
@@ -21,14 +25,23 @@ public class Ghast extends Morph implements Interactable, Flyable {
 		if (event.getItem() == null) {
 			return;
 		}
-		if (event.getItem().getType() == Material.FIRE_CHARGE) {
+		Material item = event.getItem().getType();
+		if (item == Material.FIRE_CHARGE) {
 			Player player = event.getPlayer();
 			switch (event.getAction()) {
 			case RIGHT_CLICK_AIR:
-				Location location = player.getLocation();
-				location.setY(location.getY() - 1);
-				Fireball fireball = player.launchProjectile(Fireball.class);
-				fireball.setDirection(location.getDirection());
+				long cooldownTime = 3000;
+				if (cooldownManager.getCooldownExpired(player.getUniqueId(), item, cooldownTime)) {
+					Location location = player.getLocation();
+					location.setY(location.getY() - 1);
+					Fireball fireball = player.launchProjectile(Fireball.class);
+					fireball.setDirection(location.getDirection());
+					cooldownManager.setTimeStamp(player.getUniqueId(), item);
+				} else {
+					long timeStampDifference = cooldownManager.getTimeStampDifference(player.getUniqueId(), item);
+					double remaining = (cooldownTime - timeStampDifference)/1000d;
+					player.sendMessage(ChatColor.RED + "Your ability will be availible again in " + String.format("%.2f", remaining) + " seconds.");
+				}
 				break;
 			case RIGHT_CLICK_BLOCK:
 				if (!player.isSneaking()) {
