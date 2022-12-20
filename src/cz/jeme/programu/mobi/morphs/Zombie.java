31d5a8642cd616907b61639b4e86a1f79446882d
XD_CZ <mandlemankiller@email.cz>
2022-12-20 21:07:02 +0100

    Add night vision to zombie morph

--- src/cz/jeme/programu/mobi/morphs/Zombie.java
+++ src/cz/jeme/programu/mobi/morphs/Zombie.java
@@ -3,11 +3,15 @@ package cz.jeme.programu.mobi.morphs;
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
 
 import cz.jeme.programu.mobi.interfaces.Attackable;
 import cz.jeme.programu.mobi.interfaces.Burnable;
+import cz.jeme.programu.mobi.interfaces.Effectable;
 
-public class Zombie extends Morph implements Attackable, Burnable {
+public class Zombie extends Morph implements Attackable, Burnable, Effectable {
+	
 	public Zombie() {
 		setMaxHealth(20);
 	}
@@ -17,5 +21,10 @@ public class Zombie extends Morph implements Attackable, Burnable {
 		if (player.getInventory().getItemInMainHand().getType() == Material.AIR) {
 			event.setDamage(3);
 		}
+	}
+	@Override
+	public PotionEffect[] getEffects() {
+		PotionEffect[] effects = {new PotionEffect(PotionEffectType.NIGHT_VISION, 240, 255, true, false, false)};
+		return effects;
 	}
 }
