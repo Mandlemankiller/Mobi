d94e7ecb73fa1299d5f9f34692e9274002b73383
XD_CZ <mandlemankiller@email.cz>
2022-12-21 18:29:04 +0100

    Add bat

---
+++ src/cz/jeme/programu/mobi/morphs/Bat.java
@@ -0,0 +1,26 @@
+package cz.jeme.programu.mobi.morphs;
+
+import org.bukkit.entity.Player;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
+
+import cz.jeme.programu.mobi.interfaces.Effectable;
+import cz.jeme.programu.mobi.interfaces.Flyable;
+
+public class Bat extends Morph implements Flyable, Effectable {
+	public Bat() {
+		setMaxHealth(6);
+	}
+
+	@Override
+	public PotionEffect[] getEffects() {
+		PotionEffect[] effects = {new PotionEffect(PotionEffectType.NIGHT_VISION, 12030, 255, true, false, false)};
+		return effects;
+	}
+
+	@Override
+	public void allowFlight(Player player) {
+		player.setAllowFlight(true);
+	}
+	
+}
