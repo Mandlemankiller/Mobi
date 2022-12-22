29457d25acd43a178d6ce036d654fcb38704ec12
XD_CZ <mandlemankiller@email.cz>
2022-12-22 16:33:43 +0100

    Add magma cube

---
+++ src/cz/jeme/programu/mobi/morphs/MagmaCube.java
@@ -0,0 +1,22 @@
+package cz.jeme.programu.mobi.morphs;
+
+import org.bukkit.entity.Player;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
+
+import cz.jeme.programu.mobi.interfaces.Effectable;
+
+public class MagmaCube extends SlimeCube implements Effectable {
+
+	public MagmaCube() {
+		setMaxHealth(16);
+	}
+
+	@Override
+	public PotionEffect[] getEffects(Player player) {
+		int life = Integer.parseInt(getMobiData().getMorphData(player, "life"));
+		
+		PotionEffect[] effects = { new PotionEffect(PotionEffectType.JUMP, 12030, life, true, false, false) };
+		return effects;
+	}
+}
